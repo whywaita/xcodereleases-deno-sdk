2f47ee8714d3fb28076863739821a721295aceee
whywaita <whywaita@whywrite.it>
2024-08-16 23:24:30 +0900

    Add util functions

--- mod.ts
+++ mod.ts
@@ -10,6 +10,7 @@ export async function GetXcodeReleases(): Promise<XcodeRelease[]> {
 
 /**
  * Get the Xcode releases from a URL
+ * The response sorted by _versionOrder
  * @param url
  * @returns {Promise<XcodeRelease[]>}
  */
@@ -18,7 +19,10 @@ export async function GetXcodeReleasesWithURL(
 ): Promise<XcodeRelease[]> {
   return await fetch(url)
     .then((response) => response.json())
-    .then((data) => parseXcodeReleases(data));
+    .then((data) => parseXcodeReleases(data))
+    .then((releases) =>
+      releases.sort((a, b) => a._versionOrder - b._versionOrder)
+    );
 }
 
 /**
@@ -28,4 +32,72 @@ export async function GetXcodeReleasesWithURL(
  */
 export function parseXcodeReleases(data: string): XcodeRelease[] {
   return JSON.parse(data);
+}
+
+/**
+ * Get the Xcode release by version
+ * @param releases
+ * @param version
+ * @returns XcodeRelease[]
+ */
+export function GetXcodeReleaseByVersion(
+  releases: XcodeRelease[],
+  version: string,
+): XcodeRelease | undefined {
+  return releases.find((release) => release.version.number === version);
+}
+
+/**
+ * Get the Xcode releases by version
+ * @param releases: XcodeRelease[]
+ * @param version: string
+ * @returns XcodeRelease[]
+ */
+export function GetXcodeReleasesByRelease(
+  releases: XcodeRelease[],
+  releaseType: "release" | "beta" | "rc" | "gm" | "gmSeed" | "dp",
+): XcodeRelease[] {
+  switch (releaseType) {
+    case "release":
+      return releases.filter((release) => release.version.release.release);
+    case "beta":
+      return releases.filter((release) =>
+        release.version.release.beta !== undefined
+      );
+    case "rc":
+      return releases.filter((release) =>
+        release.version.release.rc !== undefined
+      );
+    case "gm":
+      return releases.filter((release) => release.version.release.gm);
+    case "gmSeed":
+      return releases.filter((release) =>
+        release.version.release.gmSeed !== undefined
+      );
+    case "dp":
+      return releases.filter((release) =>
+        release.version.release.dp !== undefined
+      );
+  }
+}
+
+/**
+ * Get the Xcode releases by date
+ * @param releases
+ * @param date
+ * @returns XcodeRelease[]
+ */
+export function GetXcodeReleasesUntilDate(
+  releases: XcodeRelease[],
+  date: Date,
+): XcodeRelease[] {
+  return releases.filter((release) => {
+    const releaseDate = new Date(
+      release.date.year,
+      release.date.month - 1,
+      release.date.day,
+    );
+
+    return releaseDate <= date;
+  });
 }
